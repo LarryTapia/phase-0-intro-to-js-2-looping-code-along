63853e0c63f5ffc7da9073000f3a8bcef3532000
Larry Tapia <tapialarry1@gmail.com>
2023-06-30 02:41:48 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const names = [];
+function writeCards(names, eventName){
+    for (let i = 0; i < names.length; i++) {
+        names[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+
+    return names;
+}
+
+function countDown(number){
+    for(let i = number; i >= 0; i--){
+        console.log(i);
+    }
+}
